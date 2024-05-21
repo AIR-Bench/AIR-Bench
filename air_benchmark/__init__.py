94da4e969f97b89d303c305d07f1089ec1783a3a
hanhainebula <2512674094@qq.com>
2024-05-21 12:42:42 +0800

    fix bug in __init__ import

--- air_benchmark/__init__.py
+++ air_benchmark/__init__.py
@@ -1,9 +1,9 @@
-from air_benchmark import AirBench
+from air_benchmark import AIRBench
 from air_benchmark.evaluation_utils.searcher import Retriever, Reranker
 from air_benchmark.evaluation_utils.evaluation_arguments import EvalArgs
 
 __all__ = [
-    "AirBench",
+    "AIRBench",
     "Retriever",
     "Reranker",
     "EvalArgs",

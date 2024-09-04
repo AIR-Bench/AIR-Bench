d59e7bcb2f6677ecb2126174416e731d78df0e62
hanhainebula <2512674094@qq.com>
2024-09-04 17:18:44 +0800

    add evaluate_results API for backend code

--- air_benchmark/evaluation_utils/evaluator.py
+++ air_benchmark/evaluation_utils/evaluator.py
@@ -1,7 +1,8 @@
 import json
 import logging
 import os
-from typing import Dict, Optional
+from typing import Dict, Optional,List
+from mteb.evaluation.evaluators import RetrievalEvaluator
 
 from air_benchmark.evaluation_utils.data_loader import DataLoader
 from air_benchmark.evaluation_utils.searcher import Retriever, Reranker
@@ -178,3 +179,81 @@ class Evaluator:
 
         search_results = data_info.pop("search_results")
         return data_info, search_results
+
+    @staticmethod
+    def compute_metrics(
+        qrels: Dict[str, Dict[str, int]],
+        search_results: Dict[str, Dict[str, float]],
+        k_values: List[int],
+    ):
+        ndcg, _map, recall, precision, _ = RetrievalEvaluator.evaluate(
+            qrels=qrels,
+            results=search_results,
+            k_values=k_values,
+            ignore_identical_ids=True,
+        )
+        mrr, _ = RetrievalEvaluator.evaluate_custom(
+            qrels=qrels,
+            results=search_results,
+            k_values=k_values,
+            metric='mrr',
+        )
+        scores = {
+            **{f"ndcg_at_{k.split('@')[1]}": v for (k, v) in ndcg.items()},
+            **{f"map_at_{k.split('@')[1]}": v for (k, v) in _map.items()},
+            **{f"recall_at_{k.split('@')[1]}": v for (k, v) in recall.items()},
+            **{f"precision_at_{k.split('@')[1]}": v for (k, v) in precision.items()},
+            **{f"mrr_at_{k.split('@')[1]}": v for (k, v) in mrr.items()},
+        }
+        return scores
+    
+    def evaluate_results(
+        self,
+        search_results_save_dir: str,
+        k_values: List[int] = [1, 3, 5, 10, 100, 1000],
+    ):
+        eval_results_dict = {}
+        
+        for task_type in os.listdir(search_results_save_dir):
+            task_type_dir = os.path.join(search_results_save_dir, task_type)
+            if not os.path.isdir(task_type_dir):
+                continue
+            for domain in os.listdir(task_type_dir):
+                domain_dir = os.path.join(task_type_dir, domain)
+                for file in os.listdir(domain_dir):
+                    file_path = os.path.join(domain_dir, file)
+                    data_info, search_results = self.load_search_results(file_path)
+                    
+                    _task_type = data_info['task_type']
+                    _domain = data_info['domain']
+                    
+                    assert task_type == _task_type, f'Mismatch task_type: {task_type} vs {_task_type} in {file_path}'
+                    assert domain == _domain, f'Mismatch domain: {domain} vs {_domain} in {file_path}'
+                    
+                    language = data_info['language']
+                    
+                    if task_type not in eval_results_dict:
+                        eval_results_dict[task_type] = {}
+                    if domain not in eval_results_dict[task_type]:
+                        eval_results_dict[task_type][domain] = {}
+                    if language not in eval_results_dict[task_type][domain]:
+                        eval_results_dict[task_type][domain][language] = {}
+                    
+                    task_name = data_info['task_name']
+                    
+                    qrels = self.data_loader.load_qrels(
+                        task_type=task_type,
+                        domain=domain,
+                        language=language,
+                        task_name=task_name
+                    )
+                    
+                    eval_results = self.compute_metrics(
+                        qrels=qrels,
+                        search_results=search_results,
+                        k_values=k_values
+                    )
+                    
+                    eval_results_dict[task_type][domain][language][task_name] = eval_results
+        
+        return eval_results_dict

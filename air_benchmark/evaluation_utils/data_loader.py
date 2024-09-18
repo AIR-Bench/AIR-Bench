d6c7bcdedf7bcd1b2859c2d2794e388f0ed63b21
hanhainebula <2512674094@qq.com>
2024-09-19 01:05:14 +0800

    update v24.05: data_loader.py split `load_data` to `load_corpus` and `load_queries`

--- air_benchmark/evaluation_utils/data_loader.py
+++ air_benchmark/evaluation_utils/data_loader.py
@@ -2,6 +2,7 @@ import os
 import logging
 import datasets
 
+
 logger = logging.getLogger(__name__)
 
 
@@ -10,37 +11,23 @@ class DataLoader:
         self.benchmark_version = benchmark_version
         self.cache_dir = cache_dir
 
-    def load_data(self, task_type: str, domain: str, language: str, task_name: str):
-        task_name = task_name.replace("-", "_")
-        corpus_data = datasets.load_dataset(
-            f"AIR-Bench/{task_type}_{domain}_{language}",
-            self.benchmark_version,
-            split=f"corpus_{task_name}",
-            cache_dir=self.cache_dir,
-        )
-        queries_data = datasets.load_dataset(
-            f"AIR-Bench/{task_type}_{domain}_{language}",
-            self.benchmark_version,
-            split=f"queries_{task_name}",
-            cache_dir=self.cache_dir,
-        )
-
-        corpus = {e["id"]: {"text": e["text"]} for e in corpus_data}
-        queries = {e["id"]: e["text"] for e in queries_data}
-
-        corpus = datasets.DatasetDict(corpus)
-        queries = datasets.DatasetDict(queries)
-        return corpus, queries
-
-    def load_qrels(self, task_type: str, domain: str, language: str, task_name: str):
-        task_name = task_name.replace('-', '_')
-        qrels_data = datasets.load_dataset(
-            f'AIR-Bench/qrels-{task_type}_{domain}_{language}',
-            self.benchmark_version,
-            split=f"qrels_{task_name}",
-            cache_dir=self.cache_dir,
-            token=os.getenv('HF_TOKEN', None),
-        )
+    def load_qrels(self, task_type: str, domain: str, language: str, dataset_name: str, split: str = 'dev'):
+        dataset_name = dataset_name.replace('-', '_')
+        if split == 'test':
+            qrels_data = datasets.load_dataset(
+                f'AIR-Bench/qrels-{task_type}_{domain}_{language}',
+                self.benchmark_version,
+                split=f"qrels_{dataset_name}_{split}" if self.benchmark_version != 'AIR-Bench_24.04' else f"qrels_{dataset_name}",        # To be compatible with the AIR-Bench_24.04 version
+                cache_dir=self.cache_dir,
+                token=os.getenv('HF_TOKEN', None),
+            )
+        else:
+            qrels_data = datasets.load_dataset(
+                f'AIR-Bench/{task_type}_{domain}_{language}',
+                self.benchmark_version,
+                split=f"qrels_{dataset_name}_{split}",
+                cache_dir=self.cache_dir,
+            )
         
         qrels = {}
         for data in qrels_data:
@@ -50,3 +37,27 @@ class DataLoader:
             qrels[qid][data['docid']] = data['relevance']
         
         return datasets.DatasetDict(qrels)
+    
+    def load_corpus(self, task_type: str, domain: str, language: str, dataset_name: str):
+        dataset_name = dataset_name.replace('-', '_')
+        corpus_data = datasets.load_dataset(
+            f'AIR-Bench/{task_type}_{domain}_{language}',
+            self.benchmark_version,
+            split=f"corpus_{dataset_name}",
+            cache_dir=self.cache_dir,
+        )
+        
+        corpus = {e['id']: {'text': e['text']} for e in corpus_data}
+        return datasets.DatasetDict(corpus)
+    
+    def load_queries(self, task_type: str, domain: str, language: str, dataset_name: str, split: str = 'test'):
+        dataset_name = dataset_name.replace('-', '_')
+        queries_data = datasets.load_dataset(
+            f'AIR-Bench/{task_type}_{domain}_{language}',
+            self.benchmark_version,
+            split=f"queries_{dataset_name}_{split}" if self.benchmark_version != 'AIR-Bench_24.04' else f"queries_{dataset_name}",    # To be compatible with the AIR-Bench_24.04 version
+            cache_dir=self.cache_dir,
+        )
+        
+        queries = {e['id']: e['text'] for e in queries_data}
+        return datasets.DatasetDict(queries)

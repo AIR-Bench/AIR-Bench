2eabd6cccefca20ed8c1d3f51680670eaaa80782
hanhainebula <2512674094@qq.com>
2024-09-27 12:03:44 +0800

    update bm25 eval scripts - adapt to v24.05 - add bm25-based reranking option

--- scripts/bm25/evaluate_bm25.py
+++ scripts/bm25/evaluate_bm25.py
@@ -21,20 +21,23 @@ from transformers import HfArgumentParser
 from air_benchmark import EvalArgs, AIRBench
 
 from utils.searcher import BM25Retriever
-from utils.arguments import BM25Args
+from utils.arguments import BM25Args, RerankerArgs
+from utils.models import Reranker
 
 
 def main():
-    parser = HfArgumentParser([BM25Args, EvalArgs])
-    bm25_args, eval_args = parser.parse_args_into_dataclasses()
+    parser = HfArgumentParser([BM25Args, EvalArgs, RerankerArgs])
+    bm25_args, eval_args, reranker_args = parser.parse_args_into_dataclasses()
     bm25_args: BM25Args
     eval_args: EvalArgs
+    reranker_args: RerankerArgs
     
     evaluation = AIRBench(
         benchmark_version=eval_args.benchmark_version,
         task_types=eval_args.task_types,
         domains=eval_args.domains,
         languages=eval_args.languages,
+        splits=eval_args.splits,
         cache_dir=eval_args.cache_dir,
     )
     
@@ -44,8 +47,19 @@ def main():
         search_top_k=eval_args.search_top_k,
     )
     
+    reranker = Reranker(
+        reranker_args.model_name_or_path,
+        use_fp16=reranker_args.use_fp16,
+        query_instruction_for_reranking=reranker_args.query_instruction_for_reranking,
+        passage_instruction_for_reranking=reranker_args.passage_instruction_for_reranking,
+        max_length=reranker_args.max_length,
+        batch_size=reranker_args.batch_size,
+        trust_remote_code=reranker_args.trust_remote_code,
+    )
+    
     evaluation.run(
         retriever,
+        reranker=reranker,
         output_dir=eval_args.output_dir,
         overwrite=eval_args.overwrite,
     )
--- scripts/bm25/utils/arguments.py
+++ scripts/bm25/utils/arguments.py
@@ -10,3 +10,27 @@ class BM25Args:
     remove_bm25_tmp_dir: bool = field(
         default=True, metadata={"help": "Remove BM25 tmp dir or not."}
     )
+
+
+@dataclass
+class RerankerArgs:
+    model_name_or_path: str = field(
+        default=None,
+        metadata={
+            "help": 'One or more reranker name or path. For example, "BAAI/bge-reranker-v2-m3" or "path/to/reranker".',
+        },
+    )
+    add_instruction: bool = field(default=False, metadata={"help": "Add instruction for reranking?"})
+    query_instruction_for_reranking: str = field(
+        default=None, metadata={"help": "query instruction for reranking"}
+    )
+    passage_instruction_for_reranking: str = field(
+        default=None, metadata={"help": "passage instruction for reranking"}
+    )
+    max_length: int = field(default=512, metadata={"help": "Max length for reranking."})
+    batch_size: int = field(default=256, metadata={"help": "Inference batch size for reranking."})
+    trust_remote_code: bool = field(
+        default=False, metadata={"help": "Trust remote code or not."}
+    )
+
+    use_fp16: bool = field(default=True, metadata={"help": "Use fp16 for reranking."})
